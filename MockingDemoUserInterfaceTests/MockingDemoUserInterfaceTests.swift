507634c04f42c63574d7ac644a3079b29323114e
Bikram Aryal <bikram.aryall@gmail.com>
2022-05-19 17:46:02 +0545

    Writing Test to Validate Required Fields

---
+++ MockingDemoUserInterfaceTests/MockingDemoUserInterfaceTests.swift
@@ -0,0 +1,38 @@
+//
+//  MockingDemoUserInterfaceTests.swift
+//  MockingDemoUserInterfaceTests
+//
+//  Created by Bikram Aryal on 19/05/2022.
+//
+
+import XCTest
+
+class when_user_clicks_on_login_button: XCTestCase {
+    
+    func test_should_display_error_message_for_missing_required_fields() {
+        let app = XCUIApplication()
+        continueAfterFailure = false
+        app.launch()
+        
+        
+        let usernameTextField = app.textFields["usernameTextField"]
+        usernameTextField.tap()
+        
+        usernameTextField.typeText("")
+        
+        let passwordTextField = app.textFields["passwordTextField"]
+        passwordTextField.tap()
+        passwordTextField.typeText("")
+        
+        let loginButton = app.buttons["loginButton"]
+        loginButton.tap()
+        
+        let messageText = app.staticTexts["messageText"]
+        
+        
+        XCTAssertEqual(messageText.label, "Required fields are missing")
+        
+    }
+    
+   
+}
